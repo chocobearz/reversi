ed8a66ec6bceb245d5401e842da80c05594902bc
ptuttosi <ptuttosi@sfu.ca>
2020-11-02 08:57:18 -0800

    add playout timing in julia

--- chooseMovejl.jl
+++ chooseMovejl.jl
@@ -58,89 +58,102 @@ function chooseMove(
         end
         player = 1-player
       end
+
+      # all subsequent plays random for both players until game over
+      #= time the while loop, majority of computation is here, the rest is
+      fairly negligible =#
+      loopCounter = 0
+      time = @timed while won != true
+        play_choices = []
+        # choose randomly from empty locations
+        play_choices = getPlays(current_board)
+        if isassigned(play_choices, 1) == false
+          if passed
+            won = true
+          else
+            passed = true
+          end
+          player = 1-player
+          continue
+        end
+        temp_possible_boards = play_choices[2]
+        #pure MCTS
+        if difficulty == 1
+          chosen = rand(1:size(temp_possible_boards)[1])
+        #use gameplay tactics
+        else
+          bestScore = -Inf
+          chosen = 0
+          for i in 1:size(temp_possible_boards)[1]
+            score= finalHeuristicjl(temp_possible_boards[i,:,:],player,moves,valid)
+            if score>bestScore
+              chosen=i
+            end
+          end
+        end
+        current_board = temp_possible_boards[chosen,:,:]
+        player = 1-player
+        mustPass = true
+        for x in 1:8
+          for y in 1:8
+            if valid(current_board,player,x-1,y-1)
+              mustPass=false
+            end
+          end
+        end
+        if mustPass
+          if passed
+            won = true
+          else
+            passed = true
+          end
+          player = 1-player
+        end
+        loopCounter +=1
+      end
+      # in order to account for some plays having more or less choices
+      if loopCounter > 0
+        append!(loopTime, (time[2]/(loopCounter)))
+      else
+        append!(loopTime, 0)
+      end
+      #flat_board = Iterators.flatten(current_board)
+      #println(flat_board)
+      tiles = ['w','b']
+      tile_counts=Dict([(i,count(x->x==i,current_board)) for i in tiles])
+      #number of black tiles
+      if haskey(tile_counts, 'b')
+        black = tile_counts['b']
+      else
+        black = 0
+      end
+      #number of white tiles
+      if haskey(tile_counts, 'w')
+        white = tile_counts['w']
+      else
+        white = 0
+      end
+      #increment the relevant stat
+      #allowe AI to play against eachother
+      if current_player == 1
+        if black > white
+          wins += 1
+        elseif black == white
+          draws += 1
+        elseif black < white
+          losses += 1
+        end
+      else
+        if white > black
+          wins += 1
+        elseif black == white
+          draws += 1
+        elseif white < black
+          losses += 1
+        end
+      end
     end
 
-    # all subsequent plays random for both players until game over
-    while won != true
-      play_choices = []
-      # choose randomly from empty locations
-      play_choices = getPlays(current_board)
-      if isassigned(play_choices, 1) == false
-        if passed
-          won = true
-        else
-          passed = true
-        end
-        player = 1-player
-        continue
-      end
-      temp_possible_boards = play_choices[2]
-      #pure MCTS
-      if depth == 1
-        chosen = rand(1:size(temp_possible_boards)[1])
-      #use gameplay tactics
-      else
-        bestScore = -Inf
-        chosen = 0
-        for i in 1:size(temp_possible_boards)[1]
-          score= finalHeuristicjl(temp_possible_boards[i,:,:],player,moves,valid)
-          if score>bestScore
-            chosen=i
-          end
-        end
-      end
-      current_board = temp_possible_boards[chosen,:,:]
-      player = 1-player
-      mustPass = true
-      for x in 1:8
-        for y in 1:8
-          if valid(current_board,player,x-1,y-1)
-            mustPass=false
-          end
-        end
-      end
-      if mustPass
-        if passed
-          won = true
-        else
-          passed = true
-        end
-        player = 1-player
-      end
-    end
-    #flat_board = Iterators.flatten(current_board)
-    #println(flat_board)
-    tiles = ['w','b']
-    tile_counts=Dict([(i,count(x->x==i,current_board)) for i in tiles])
-    #number of black tiles
-    if haskey(tile_counts, 'b')
-      black = 0
-    end
-    #number of white tiles
-    if haskey(tile_counts, 'w')
-      white = tile_counts['w']
-    else
-      white = 0
-    end
-    #increment the relevant stat
-    #allowe AI to play against eachother
-    if current_player == 1
-      if black > white
-        wins += 1
-      elseif black == white
-        draws += 1
-      elseif black < white
-        losses += 1
-      end
-    else
-      if white > black
-        wins += 1
-      elseif black == white
-        draws += 1
-      elseif white < black
-        losses += 1
-      end
-    end
     #print(
     # "square: {}, wins: {}, draws: {}, losses : {}".format(
     # empty_location,wins, draws, losses
